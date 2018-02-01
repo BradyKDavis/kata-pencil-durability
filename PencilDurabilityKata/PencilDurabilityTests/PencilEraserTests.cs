53b1381107f0183dc8665a843fac48cdd5579618
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-01 12:05:13 -0600

    Adding initial test for pencil eraser

---
+++ PencilDurabilityKata/PencilDurabilityTests/PencilEraserTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using PencilDurabilityKata.Kata;
+namespace PencilDurabilityTests
+{
+    [TestFixture]
+    public class PencilEraserTests
+    {
+        private Pencil sut;
+
+        private Paper paper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            paper = new Paper();
+        }
+
+        [Test]
+        public void WhenPencilErasesWordFromPaperThatWordIsReplacedWithSpaces()
+        {
+            sut = new Pencil();
+            sut.Write(paper, "This is a cool sentence.");
+            sut.Erase(paper, "cool");
+            Assert.AreEqual(paper.Text, "This is a      sentence.");
+        }
+    }
+}
