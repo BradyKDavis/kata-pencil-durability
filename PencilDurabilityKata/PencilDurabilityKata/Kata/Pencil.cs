c3369edd40daf6f91bf2c0a7c59b57ae7b922e9b
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-01 09:39:46 -0600

    Sharpening a pencil now returns its durability to its initial durability

--- PencilDurabilityKata/PencilDurabilityKata/Kata/Pencil.cs
+++ PencilDurabilityKata/PencilDurabilityKata/Kata/Pencil.cs
@@ -7,22 +7,28 @@ namespace PencilDurabilityKata.Kata
         private const char EMPTY = ' ';
         private const char NEWLINE = '\n';
 
+        private int _initialDurability;
         private int _durability;
 
         public Pencil()
         {
-            _durability = MAX_DURABILITY;
+            _durability = _initialDurability = MAX_DURABILITY;
         }
 
         public Pencil(int initialDurability)
         {
-            _durability = initialDurability;
+            _durability = _initialDurability = initialDurability;
         }
 
         public void Write(Paper paper, String text)
         {
             String appliedText = applyDurabilityToWriting(text);
             paper.Text = paper.Text + appliedText;
+        }
+
+        public void Sharpen()
+        {
+            _durability = _initialDurability;
         }
 
         private String applyDurabilityToWriting(String text)
