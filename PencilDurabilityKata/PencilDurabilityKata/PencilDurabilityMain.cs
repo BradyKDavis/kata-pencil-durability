ba5f00015d450c82eb41c56efb86f72d85fbffb3
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-01-31 19:15:55 -0600

    Initial Setup of Kata.
    Copy/Pasted initial  .gitIgnore from https://github.com/github/gitignore/blob/master/VisualStudio.gitignore to ignore general clutter files from visual studio

---
+++ PencilDurabilityKata/PencilDurabilityKata/PencilDurabilityMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PencilDurabilityKata
+{
+    class PencilDurabilityMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
