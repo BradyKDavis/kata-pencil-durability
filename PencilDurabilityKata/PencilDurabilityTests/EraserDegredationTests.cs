235fbf15ff4ba9276b504810afefd4ad52f5bdbc
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-01 13:14:52 -0600

    Adding initial test for Exception throwing on negative eraser durability

---
+++ PencilDurabilityKata/PencilDurabilityTests/EraserDegredationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using PencilDurabilityKata.Kata;
+namespace PencilDurabilityTests
+{
+    [TestFixture]
+    public class EraserDegredationTests
+    {
+        private Pencil sut;
+
+        private Paper paper;
+
+        [SetUp]
+        public void init()
+        {
+            paper = new Paper();
+        }
+
+        [Test]
+        public void TestThatWhenEraserIsGivenNegativeDurabilityArgumentErrorIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => sut = new Pencil(int.MaxValue, int.MaxValue, -1));
+        }
+    }
+}
