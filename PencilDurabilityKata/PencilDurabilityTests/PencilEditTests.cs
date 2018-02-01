887d37874179e4e94d1e43a1462eae8f1cec4362
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-02-01 15:08:34 -0600

    Initial pencil edit test code

---
+++ PencilDurabilityKata/PencilDurabilityTests/PencilEditTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using PencilDurabilityKata.Kata;
+namespace PencilDurabilityTests
+{
+    [TestFixture]
+    public class PencilEditTests
+    {
+        private Pencil sut;
+
+        private Paper paper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new Pencil();
+            paper = new Paper();
+        }
+
+        [Test]
+        public void TestEditOverEmptySpacePlacesStringInThatSpace()
+        {
+            sut.Write(paper, "     is some space in which to put a word.");
+            sut.Edit(paper, "Here", 0);
+            Assert.AreEqual("Here is some space in which to put a word.", paper.Text);
+        }
+    }
+}
