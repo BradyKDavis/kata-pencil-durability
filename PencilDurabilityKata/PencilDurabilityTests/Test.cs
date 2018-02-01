cf73f80445d021229e3f16603476efdad77daa26
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-01-31 20:35:22 -0600

    Adding Initial Fail Test and Hello World Main

---
+++ PencilDurabilityKata/PencilDurabilityTests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace PencilDurabilityTests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+            Assert.Fail();
+        }
+    }
+}
